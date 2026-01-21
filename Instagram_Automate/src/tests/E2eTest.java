0f50b091ad2412c013ae3404f763de80dcdb1f9b
Venkatesh KP <Venkatesh KP@Yadav>
2026-01-21 15:34:41 +0530

    test page changes

--- Instagram_Automate/src/tests/E2eTest.java
+++ Instagram_Automate/src/tests/E2eTest.java
@@ -44,6 +44,7 @@ public class E2eTest extends BaseTest {
 		h.search1Profile();
 		fr.followRequest();
 		// h.profileData();
+		System.out.println("search is done");
 	}
 
 //	@Test
@@ -138,7 +139,7 @@ public class E2eTest extends BaseTest {
 		for (int i1 = 0; i1 < 5; i1++) {
 			e.like();
 			e.back();
-		}
+		} 
 		e.exploreClose();
 		System.out.println("explore operation is done");
 	}
